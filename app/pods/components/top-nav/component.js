89f3811a45e81f2517d690abaf6419367a66da41
Brian Du <brian.bh.du@gmail.com>
2018-09-19 14:53:17 -0400

    Temp

--- app/pods/components/top-nav/component.js
+++ app/pods/components/top-nav/component.js
@@ -1,5 +1,18 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
-  tagName: 'top-nav'
+	tagName: 'top-nav',
+
+	logoUrl: '/assets/eventr-logo.svg',
+
+	actions: {
+		setActive() {
+			this.set('logoUrl', '/assets/eventr-logo-animated.gif');
+		},
+
+		setInactive() {
+			this.set('logoUrl', '/assets/eventr-logo.svg');
+		}
+	}
 });
